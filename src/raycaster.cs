4230c4b8c4acd43f225280ba3bc60a01037827ee
roomunrecalled <yhorng@zoho.com>
2018-03-31 22:06:32 -0500

    added loadLevel() method that converts tilemap nodes to 2D arrays

--- src/raycaster.cs
+++ src/raycaster.cs
@@ -9,24 +9,29 @@ public class raycaster : Node2D
     "Required child nodes not found. Please make sure both a Sprite node" +
     " called 'Player' and a TileMap node called 'Level' are attached.";
 
+    // This can probably change on the fly, but I don't recommend it.
+    private Vector2 screenDimensions;
+
     private bool childMessage;
     private bool ready;
 
-    private Sprite player;
-    private TileMap level;
+    private int[,] level;
 
+    private Vector2 playerPosition;
+    private float playerRotation;
+
+    // Called every time the node is added to the scene.
     public override void _Ready()
     {
-        // Called every time the node is added to the scene.
-        // Initialization here
-        if (debug) {
-            GD.Print("DEBUG: Raycaster initialized.");
-        }
-        childMessage = false;
+        screenDimensions = GetViewport().Size;
 
+        childMessage = false;
         ready = loadRequiredChildren();
 
         testMethod();
+        if (debug) {
+            GD.Print("DEBUG: Raycaster initialized.");
+        }
     }
 
     public override void _Draw()
@@ -40,24 +45,18 @@ public class raycaster : Node2D
         Update();
     }
 
-    private void testMethod()
-    {
-        draw();
-    }
-
-    private void drawTest() 
-    {
-        DrawRect(new Rect2(new Vector2(0,0), new Vector2(30,30)), new Color(0,244,0));
-    }
-
     private void draw()
     {
+        // If the player and level aren't loaded there's no point.
+        if (!ready) return;
+
+        // Call the actual draw function.
     }
 
     private bool loadRequiredChildren()
     {
-        player = (Sprite) GetNode("Player");
-        level = (TileMap) GetNode("Level");
+        var player = (Sprite) GetNode("Player");
+        var level = (TileMap) GetNode("Level");
 
         bool result = true;
 
@@ -71,11 +70,55 @@ public class raycaster : Node2D
                 childMessage = true;
             }
             result = false;
+        } 
+        else
+        {
+            // Generate a matrix representation of the level.
+
+            // Retrieve the player position & orientation.
+            playerPosition = player.GetPosition();
+            playerRotation = player.GetRotation();
         }
+
         if (debug && !childMessage) {
             GD.Print("Player and Level nodes loaded.");
             childMessage = true;
         }
         return result;
     }
+
+    private int[,] loadLevel(TileMap tilemap) 
+    {
+        // Calculate tilemap dimensions
+        Rect2 levelRect = tilemap.GetUsedRect();
+        Vector2 levelDimensions = levelRect.Size;
+
+        var level = new int[(int) levelDimensions.x, (int) levelDimensions.y];
+        // initialize all elements in the created array to -1
+        for (int i = 0; i < levelDimensions.x * levelDimensions.y; i += 1)
+        {
+            level[i%(int) levelDimensions.x, i/(int) levelDimensions.x] = -1;
+        }
+
+        foreach (Vector2 tilePosition in tilemap.GetUsedCells())
+        {
+            int tilesetIndex = tilemap.GetCellv(tilePosition);
+            level[(int) tilePosition.x,(int) tilePosition.y] = tilesetIndex;
+        }
+
+        return level;
+    }
+
+    private void testMethod()
+    {
+        //draw();
+        loadLevel((TileMap) GetNode("Level"));
+    }
+
+    private void drawTest() 
+    {
+        DrawRect(new Rect2(new Vector2(0,0), new Vector2(30,30)), new Color(0,244,0));
+    }
+
+
 }
